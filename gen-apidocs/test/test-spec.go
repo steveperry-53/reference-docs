d4148dfac8c573ab46b64c577cddc28643dacba2
steveperry-53 <seperry53@gmail.com>
2017-10-25 18:17:21 +0000

    Copy test spec.

--- gen-apidocs/test/test-spec.go
+++ gen-apidocs/test/test-spec.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	fmt.Println("Hello: test-spec")
 
-	_, err := loads.JSONSpec("openapi-spec/swagger-test.json")
+	//_, err := loads.JSONSpec("/home/seperry53/src/github.com/kubernetes-incubator/reference-docs/gen-apidocs/generators/openapi-spec/swagger.json") This works.	
 	
+	_, err := loads.JSONSpec("openapi-spec/swagger-test.json")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	} else {
